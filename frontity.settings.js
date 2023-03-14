6790c41139b671dbfb66122f5836b03bde6e2166
Walter Clayton <walter.h.clayton@gmail.com>
2023-03-14 10:09:51 +0100

    first commit

---
+++ frontity.settings.js
@@ -0,0 +1,68 @@
+const settings = {
+  "name": "my-website",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/walter_clayton-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Portfolio",
+              "/project/"
+            ],
+            [
+              "About",
+              "/about/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "api": "https://walter-clayton.000webhostapp.com/wp-json",
+          "postTypes": [
+            {
+              type: "project",
+              endpoint: "project",
+              archive: "/project"
+            }
+          ],
+          taxonomies: [
+            {
+              taxonomy: "project_hard_skills",
+              endpoint: "project_hard_skills",
+              postTypeEndpoint: "project"
+            },
+            {
+              taxonomy: "project_soft_skills",
+              endpoint: "project_soft_skills",
+              postTypeEndpoint: "project"
+            }
+          ]
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
