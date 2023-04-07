afec0ca53a558d190afc56510cd038d118160166
Walter Clayton <walter.h.clayton@gmail.com>
2023-04-07 12:58:12 +0200

    added animation about page

--- packages/walter_clayton-theme/src/components/about.js
+++ packages/walter_clayton-theme/src/components/about.js
@@ -1,12 +1,15 @@
 import { styled, css } from "frontity";
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import profileImage from "../../../../static/images/profile.png";
 import inspirationImage from "../../../../static/images/brancusi.png";
-import Review from "../components/review"
-import data from '../../../../static/data/reviews.json';
+import Review from "../components/review";
+import data from "../../../../static/data/reviews.json";
 
 const About = () => {
+  const sectionOneRef = useRef(null); // Define sectionOneRef here
+  const sectionTwoRef = useRef(null); // Define sectionTwoRef here
+  const sectionThreeRef = useRef(null); // Define sectionTwoRef here
+
   const [reviews, setReviews] = useState(data);
   const [index, setIndex] = useState(0);
   const [reviewIndex, setReviewIndex] = useState(0);
@@ -21,26 +24,77 @@ const About = () => {
     setIndex((index + 1) % reviews.length);
     setReviewIndex((reviewIndex + 1) % reviews.length);
   };
+  
+  const [isSectionOneVisible, setIsSectionOneVisible] = useState(false);
+  const [isSectionTwoVisible, setIsSectionTwoVisible] = useState(false);
+  const [isSectionThreeVisible, setIsSectionThreeVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.target === sectionOneRef.current) {
+          setIsSectionOneVisible(entry.isIntersecting);
+        }
+        if (entry.target === sectionTwoRef.current) {
+          setIsSectionTwoVisible(entry.isIntersecting);
+        }
+        if (entry.target === sectionThreeRef.current) {
+          setIsSectionThreeVisible(entry.isIntersecting);
+        }
+      },
+      {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5,
+      }
+    );
+    
+    if (sectionOneRef.current) {
+      observer.observe(sectionOneRef.current);
+    }
+    
+    if (sectionTwoRef.current) {
+      observer.observe(sectionTwoRef.current);
+    }
+
+    if (sectionThreeRef.current) {
+      observer.observe(sectionThreeRef.current);
+    }
+    
+    return () => {
+      if (sectionOneRef.current) {
+        observer.unobserve(sectionOneRef.current);
+      }
+      
+      if (sectionTwoRef.current) {
+        observer.unobserve(sectionTwoRef.current);
+      }
+      if (sectionThreeRef.current) {
+        observer.unobserve(sectionThreeRef.current);
+      }
+    };
+  }, []);
+
 
   return (
     <AboutMain>
       <SectionOne>
-        <ContainerParent>
-          <OrderOne>
-            <h1>Walter Clayton</h1>
-            <p>Turning abstract ideas into clean and simple solutions.</p>   
-          </OrderOne>
-          <OrderTwo>
-            <Image src={profileImage} alt="walter clayton profile picture" />
-          </OrderTwo>
-        </ContainerParent>
+        <ContainerParent ref={sectionOneRef} isVisible={isSectionOneVisible}>
+            <OrderOne>
+              <h1>Walter Clayton</h1>
+              <p>Turning abstract ideas into clean and simple solutions.</p>   
+            </OrderOne>
+            <OrderTwo>
+              <Image src={profileImage} alt="walter clayton profile picture" />
+            </OrderTwo>
+          </ContainerParent>
       </SectionOne>
       <SectionOneSvg preserveAspectRatio="xMidYMin slice" width="100%" height="100%" viewBox="0 0 1513 92" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
         <path d="M1512.12 1.20728H0.115723L1512.12 91.61V1.20728Z" fill="#FECBB8" />
         <path d="M0.115723 90.8214H1512.12L0.115723 0.418701V90.8214Z" fill="#507A95" />
       </SectionOneSvg>
       <SectionTwo>
-        <ContainerParent>
+        <ContainerParent ref={sectionTwoRef} isVisible={isSectionTwoVisible}>
           <OrderOne>
             <Image src={inspirationImage} alt="Brancusi" />
           </OrderOne>
@@ -67,7 +121,7 @@ const About = () => {
         <path d="M835 91H-3.85046e-05L835 0.540009V91Z" fill="white"/>
         </SectionTwoSvg>
         <SectionThree>
-          <ContainerParent>
+          <ContainerParent ref={sectionThreeRef} isVisible={isSectionThreeVisible}>
             <ContainerChild>
               <ContainerReview>
                 <h4>Reviews</h4>
@@ -139,6 +193,8 @@ const ContainerParent = styled.div`
   ${mq[1]} {
     flex-direction: column;
   }
+  opacity: ${({ isVisible }) => (isVisible ? "1" : "0")};
+  transition: opacity 0.5s ease-in-out;
 `;
 const ContainerChild = styled.div`
   align-items: center;
